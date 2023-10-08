73a120a6c4cbed0f174c0011e7062d92a7b11aa1
sfrxj123H <sfrxj123H@gmail.com>
2023-10-08 15:47:46 +0700

    update

--- CSB/index1.py
+++ CSB/index1.py
@@ -54,16 +54,41 @@
 
 import math
 total = 0
-num = int(math.floor(float(input("Type a number"))) + 1)
+lists = []
+num = int(math.floor(float(input("Type a number: "))) + 1)
 if (num <= 0):
    print("Error, please type a number that is not less than 0 !")
 if (0 < num <= 1000):
+   print("Numbers that could divide by 7")
    for ii in range(0, num):
       if (ii % 7 == 0):
          total += ii
+         lists.append(ii)
 if (1000 < num <= 2000):
-   for ii in range(0, num):
-      if (ii % 5 == 0):
-         total += ii
+   print("Numbers that could divide by 5")
+   for iii in range(0, num):
+      if (iii % 5 == 0):
+         total += iii
+         lists.append(ii)
 if (num > 2000):
    print("Error, please type a number that is less than 2000 !")
+print("total: ", total)
+print(lists)
+for ab in lists:
+   printNum = input("Select the order of the number of the list to print: ")
+   printNum = float(printNum)
+   if (printNum %1 ==0):
+      printNum = int(printNum)
+      if (abs(printNum) < len(lists) or -abs(printNum + 1) > -len(lists)):
+         print(lists[printNum])
+         askRemove = bool(input("Do you want to remove the selected number? (return empty for no): "))
+         if (askRemove == True):
+            print(f"Number {lists[printNum]} successfully removed!")
+            total -= lists[printNum]
+            lists.remove(lists[printNum])
+            print("new total: ", total)
+            print(lists)
+      else:
+         print("Input number must be in it's range!")
+   else:
+      print("List order must be an integer!")
